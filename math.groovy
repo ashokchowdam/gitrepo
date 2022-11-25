9fd631e3962dd5324f471c09061364d94851a441
ashokchowdam <118465670+ashokchowdam@users.noreply.github.com>
2022-11-25 14:54:35 +0530

    Update math.groovy

--- math.groovy
+++ math.groovy
@@ -4,9 +4,10 @@ def sum(a,b)
 }
 def subtraction(a,b)
 {
-   echo "the difference of ${a} and ${b} is {a-b}
+   echo "the difference of ${a} and ${b} is {a-b}"
  }
  def mul(a,b)
  {
-   echo "the multi of ${a} and ${b} is {a*b}
+   echo "the multi of ${a} and ${b} is {a*b}"
   
+ }
