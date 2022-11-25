f84267dfb22dc400cabce63d3a71bc4fc58ae054
ashokchowdam <118465670+ashokchowdam@users.noreply.github.com>
2022-11-25 14:53:16 +0530

    Create math.groovy

---
+++ math.groovy
@@ -0,0 +1,12 @@
+def sum(a,b)
+{
+   echo "The sum of $ {a} and ${b} sum {a+b}"
+}
+def subtraction(a,b)
+{
+   echo "the difference of ${a} and ${b} is {a-b}
+ }
+ def mul(a,b)
+ {
+   echo "the multi of ${a} and ${b} is {a*b}
+  
